8786d43b3d84727b9dd3e58b8f09353d3cf4bd33
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2020-02-25 09:53:57 +0000

    Fixing line

--- Athenaeum/Views/Unwrap.swift
+++ Athenaeum/Views/Unwrap.swift
@@ -10,8 +10,7 @@ struct Unwrap<Value, Content: View>: View {
     private let value: Value?
     private let contentProvider: (Value) -> Content
 
-    init(_ value: Value?,
-         @ViewBuilder content: @escaping (Value) -> Content) {
+    init(_ value: Value?, @ViewBuilder content: @escaping (Value) -> Content) {
         self.value = value
         self.contentProvider = content
     }
