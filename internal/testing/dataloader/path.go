7bde819118d2346a7acab3e50f9c367ec7715e31
Callum Kerson <12272439+CallumKerson@users.noreply.github.com>
2023-03-16 21:46:43 +0000

    test: consolidating test m4bs (#15)

---
+++ internal/testing/dataloader/path.go
@@ -0,0 +1,28 @@
+package dataloader
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func GetRootPath(t *testing.T) string {
+	dir, err := os.Getwd()
+	assert.NoError(t, err)
+	for _, err := os.ReadFile(filepath.Join(dir, "go.mod")); err != nil && len(dir) > 1; {
+		dir = filepath.Dir(dir)
+		_, err = os.ReadFile(filepath.Join(dir, "go.mod"))
+	}
+	if len(dir) < 2 {
+		t.Fail()
+	}
+	return dir
+}
+
+func GetRootTestdata(t *testing.T, subpaths ...string) string {
+	baseTestData := []string{GetRootPath(t), "testdata"}
+	baseTestData = append(baseTestData, subpaths...)
+	return filepath.Join(baseTestData...)
+}
