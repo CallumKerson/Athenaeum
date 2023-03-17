0e6d8b3bf57a7fda867620c221a5e6b5dd71795b
Callum Kerson <12272439+CallumKerson@users.noreply.github.com>
2023-03-17 09:17:36 +0000

    test: using baloo for http assertions (#17)

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/CallumKerson/loggerrific v1.0.1
 	github.com/CallumKerson/podcasts v0.0.5
 	github.com/alfg/mp4 v0.0.0-20210728035756-55ea58c08aeb
-	github.com/avast/retry-go v3.0.0+incompatible
+	github.com/carlmjohnson/requests v0.23.2
 	github.com/gomarkdown/markdown v0.0.0-20221013030248-663e2500819c
 	github.com/gorilla/mux v1.8.0
 	github.com/pelletier/go-toml/v2 v2.0.7
@@ -15,8 +15,10 @@ require (
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.2
+	github.com/ybbus/httpretry v1.0.2
 	go.etcd.io/bbolt v1.3.7
 	golang.org/x/time v0.3.0
+	gopkg.in/h2non/baloo.v3 v3.1.0
 )
 
 require (
@@ -26,14 +28,20 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/h2non/gentleman.v2 v2.0.5 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/transport/http/handler_test.go
+++ internal/transport/http/handler_test.go
@@ -3,11 +3,10 @@ package http
 import (
 	"context"
 	"io"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/baloo.v3"
 
 	"github.com/CallumKerson/loggerrific/tlogger"
 )
@@ -23,39 +22,32 @@ func TestHandler(t *testing.T) {
 	testServer := httptest.NewServer(testHandler)
 	defer testServer.Close()
 
-	newReq := func(method, url string, body io.Reader) *http.Request {
-		r, err := http.NewRequestWithContext(context.TODO(), method, url, body)
-		if err != nil {
-			t.Fatal(err)
-		}
-		return r
-	}
-
 	tests := []struct {
 		name                string
-		r                   *http.Request
+		method              string
+		path                string
 		expectedStatus      int
 		expectedContentType string
 		expectedBody        string
 	}{
-		{name: "health check", r: newReq("GET", testServer.URL+"/health", nil), expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"health\": \"ok\"\n}\n"},
-		{name: "readiness check", r: newReq("GET", testServer.URL+"/ready", nil), expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"readiness\": \"ok\"\n}\n"},
-		{name: "version", r: newReq("GET", testServer.URL+"/version", nil), expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"version\": \"1.0.0-test\"\n}\n"},
-		{name: "feed", r: newReq("GET", testServer.URL+"/podcast/feed.rss", nil), expectedStatus: 200, expectedContentType: ContentTypeXML, expectedBody: testFeed},
-		{name: "media", r: newReq("GET", testServer.URL+"/media/media.txt", nil), expectedStatus: 200, expectedContentType: "text/plain; charset=utf-8", expectedBody: "served file\n"},
-		{name: "update", r: newReq("POST", testServer.URL+"/update", nil), expectedStatus: 204, expectedContentType: "", expectedBody: ""},
-		{name: "update on get fails", r: newReq("GET", testServer.URL+"/update", nil), expectedStatus: 405, expectedContentType: "text/plain; charset=utf-8", expectedBody: "Method Not Allowed\n"},
+		{name: "health check", method: "GET", path: "/health", expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"health\": \"ok\"\n}\n"},
+		{name: "readiness check", method: "GET", path: "/ready", expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"readiness\": \"ok\"\n}\n"},
+		{name: "version", method: "GET", path: "/version", expectedStatus: 200, expectedContentType: ContentTypeJSON, expectedBody: "{\n  \"version\": \"1.0.0-test\"\n}\n"},
+		{name: "feed", method: "GET", path: "/podcast/feed.rss", expectedStatus: 200, expectedContentType: ContentTypeXML, expectedBody: testFeed},
+		{name: "media", method: "GET", path: "/media/media.txt", expectedStatus: 200, expectedContentType: "text/plain; charset=utf-8", expectedBody: "served file\n"},
+		{name: "update", method: "POST", path: "/update", expectedStatus: 204, expectedContentType: "", expectedBody: ""},
+		{name: "update on get fails", method: "GET", path: "/update", expectedStatus: 405, expectedContentType: "text/plain; charset=utf-8", expectedBody: "Method Not Allowed\n"},
 	}
 	for _, testCase := range tests {
 		t.Run(testCase.name, func(t *testing.T) {
-			resp, err := http.DefaultClient.Do(testCase.r)
-			assert.NoError(t, err)
-			defer resp.Body.Close()
-			b, err := io.ReadAll(resp.Body)
-			assert.NoError(t, err)
-			assert.Equal(t, testCase.expectedStatus, resp.StatusCode)
-			assert.Equal(t, testCase.expectedContentType, resp.Header.Get(ContentTypeHeader))
-			assert.Equal(t, testCase.expectedBody, string(b))
+			baloo.New(testServer.URL).
+				Method(testCase.method).
+				Path(testCase.path).
+				Request().
+				Expect(t).
+				Status(testCase.expectedStatus).
+				Type(testCase.expectedBody).
+				BodyEquals(testCase.expectedBody)
 		})
 	}
 }
