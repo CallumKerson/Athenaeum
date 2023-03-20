53ecd9f754d55f5ce0d3f772bea57ebef2face2e
Callum Kerson <12272439+CallumKerson@users.noreply.github.com>
2023-03-20 09:08:40 +0000

    refactor: adding extra podcast functions

--- internal/podcasts/service/service.go
+++ internal/podcasts/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	"github.com/CallumKerson/loggerrific"
@@ -12,10 +13,12 @@ import (
 const (
 	allAudiobooksFeedTitle       = "Audiobooks"
 	allAudiobooksFeedDescription = "Like movies in your mind!"
+	genreFeedDescriptionFormat   = "%s Audiobooks"
 )
 
 type AudiobooksClient interface {
 	GetAllAudiobooks(ctx context.Context) ([]audiobooks.Audiobook, error)
+	GetAudiobooksByGenre(ctx context.Context, genre audiobooks.Genre) ([]audiobooks.Audiobook, error)
 }
 
 type Service struct {
@@ -40,6 +43,25 @@ func (s *Service) WriteAllAudiobooksFeed(ctx context.Context, writer io.Writer) 
 	allAudiobooksFeedOpts := &FeedOpts{
 		Title:       allAudiobooksFeedTitle,
 		Description: allAudiobooksFeedDescription,
+		Link:        s.host,
+		ImageLink:   s.fedImageLink,
+		Explicit:    s.feedExplicit,
+		Language:    s.feedLanguage,
+		Author:      s.feedAuthor,
+		Email:       s.feedAuthorEmail,
+		Copyright:   s.feedCopyright,
+	}
+	return s.WriteFeedFromAudiobooks(ctx, books, allAudiobooksFeedOpts, writer)
+}
+
+func (s *Service) WriteGenreAudiobookFeed(ctx context.Context, genre audiobooks.Genre, writer io.Writer) error {
+	books, err := s.GetAudiobooksByGenre(ctx, genre)
+	if err != nil {
+		return err
+	}
+	allAudiobooksFeedOpts := &FeedOpts{
+		Title:       genre.String(),
+		Description: fmt.Sprintf(genreFeedDescriptionFormat, genre),
 		Link:        s.host,
 		ImageLink:   s.fedImageLink,
 		Explicit:    s.feedExplicit,
